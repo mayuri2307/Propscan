f0eb11c85eef37f6331e769e4bc80ffe8d8e97a1
Punit Jain <punit.jain@bytelearn.ai>
2023-05-22 11:54:50 +0530

    added property items

--- src/components/properties/carousel/PropertyCarousel.js
+++ src/components/properties/carousel/PropertyCarousel.js
@@ -9,27 +9,35 @@ import PropertyItem from "./PropertyItem";
 export default function Property() {
   return (
     <>
-      <Col xs="6" md>
+      <Col md="1" />
+      <Col xs="6" md className='p-4'>
         <PropertyItem
-          src="images/img_fisrcharthistogram.svg"
-          text="5000 +"
-          subText="Of real Estate experience"
+          src="images/img_image_1prop.png"
+          text="0123 Boulevard Street, New Delhi"
+          subText1="3 Bedrooms | 2 Bathrooms |"
+          subText2="Size - 252 ft2"
+          price="Rs. 10,000,00.00"
         />
       </Col>
-      <Col xs="6" md>
+      <Col xs="6" md className='p-4'>
         <PropertyItem
-          src="images/img_vector.svg"
-          text="10 years +"
-          subText="Of real Estate experience"
+          src="images/img_image_2prop.png"
+          text="0123 Boulevard Street, New Delhi"
+          subText1="3 Bedrooms | 2 Bathrooms |"
+          subText2="Size - 252 ft2"
+          price="Rs. 10,000,00.00"
         />
       </Col>
-      <Col xs="6" md>
+      <Col xs="6" md className='p-4'>
         <PropertyItem
-          src="images/img_fisrmoney.svg"
-          text="100 Cr+"
-          subText="Of real Estate experience"
+          src="images/img_image_3prop.png"
+          text="0123 Boulevard Street, New Delhi"
+          subText1="3 Bedrooms | 2 Bathrooms |"
+          subText2="Size - 252 ft2"
+          price="Rs. 10,000,00.00"
         />
       </Col>
+      <Col md="1" />
     </>
   )
 }
--- src/components/properties/carousel/PropertyItem.js
+++ src/components/properties/carousel/PropertyItem.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
-export default function PropertyItem({ src, text, subText }) {
+import "./propertyCarousel.css"
+
+export default function PropertyItem({ src, text, subText1, subText2, price }) {
   return (
-    <div>
-      Item
+    <div className='p-item'>
+      <div style={{position:"relative"}}>
+        <img src={src} style={{
+          maxWidth: "100%",
+          height: "auto"
+        }} />
+        <div style={{position:"absolute", top:"8%", left:"0%"}}>
+          <span className='sell'>For Sell</span>
+        </div>
+        <div style={{position:"absolute", bottom:"4%", right:"2%"}}>
+          <button className='like'><img src="images/img_download.svg" /></button>
+        </div>
+
+      </div>
+      <div className='p-4 pb-1'>
+        <span className='text'>{text}</span>
+        <br />
+        <span className='text2'>{subText1}<br /> {subText2}</span>
+      </div>
+      <span className='price'>{price}</span>
     </div>
   )
 }
