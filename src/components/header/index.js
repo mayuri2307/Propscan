4006b727fe6611f27c507c1a377b1b5692db6c67
Punit Jain <punit.jain@bytelearn.ai>
2023-07-07 05:00:11 +0530

    added menu icon/fixed searchbar in homepage

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,8 +5,21 @@ import "./header.css";
 import Searchbar from "../searchbar";
 
 export default function Header() {
+  const toggleMenu = () => {
+    const menu = document.getElementsByClassName("header-menu")[0];
+    const body = document.body;
+    if (menu.style.height === "0vh" || menu.style.height === "") {
+      menu.style.height = "100vh";
+      body.style.height = "100vh";
+      body.style.overflow = "hidden";
+    } else {
+      menu.style.height = "0vh";
+      body.style.height = "100%";
+      body.style.overflow = "";
+    }
+  };
   return (
-    <div style={{backgroundColor:"yellow", position:"relative"}}>
+    <div style={{ position: "relative" }}>
       <div className="header">
         <div
           style={{
@@ -27,25 +40,61 @@ export default function Header() {
             <span className="header-com">.com</span>
           </div>
           <div className="header-items">
-            <span className="px-5">
+            <span className="px-4 px-lg-5">
               Sell{" "}
               <img
                 src="images/img_checkmark.svg"
                 style={{ paddingBottom: "2px" }}
               />
             </span>
-            <span className="px-5">
+            <span className="px-4 px-lg-5">
               Buy{" "}
               <img
                 src="images/img_checkmark.svg"
                 style={{ paddingBottom: "2px" }}
               />
             </span>
-            <span className="px-5">About Us</span>
-            <span className="px-5">Contact Us</span>
+            <span className="px-4 px-lg-5">About Us</span>
+            <span className="px-4 px-lg-5">Contact Us</span>
             <a href="/login">
               <button className="btn-white">Log In/Sign Up</button>
             </a>
+          </div>
+          <div className="header-menu-icon" onClick={toggleMenu}>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+          <div className="header-menu">
+            <div className="px-3 px-lg-5 pb-2 pt-4">
+              <span style={{backgroundColor:"#E8F9FD", borderRadius:"38px", padding:"8px 20px", fontSize:"14px"}}>Log In/Sign Up</span>
+              <div className="header-menu-icon float-end" onClick={toggleMenu}>
+                <div style={{backgroundColor: "#0D2855"}}></div>
+                <div style={{backgroundColor: "#0D2855"}}></div>
+                <div style={{backgroundColor: "#0D2855"}}></div>
+              </div>
+            </div>
+            <div>
+              <hr/>
+              <p>
+                Sell{" "} <img
+                  src="images/img_checkmark.svg"
+                  style={{ paddingBottom: "2px", filter: "invert(34%) sepia(10%) saturate(6025%) hue-rotate(177deg) brightness(28%) contrast(99%)" }}
+                />
+              </p>
+              <hr/>
+              <p>
+                Buy{" "} <img
+                  src="images/img_checkmark.svg"
+                  style={{ paddingBottom: "2px", filter: "invert(34%) sepia(10%) saturate(6025%) hue-rotate(177deg) brightness(28%) contrast(99%)" }}
+                />
+              </p>
+              <hr/>
+              <p>About Us</p>
+              <hr/>
+              <p>Contact Us</p>
+              <hr/>
+            </div>
           </div>
         </div>
         <div className="header-main-txt">
@@ -56,9 +105,9 @@ export default function Header() {
           <span>Rent.</span>
         </div>
       </div>
-        <div>
-          <Searchbar />
-        </div>
+      <div>
+        <Searchbar />
+      </div>
     </div>
   );
 }
--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -19,29 +19,32 @@ export default function Searchbar({defaultClass="searchbar-setter"}) {
   return (
     <div className={defaultClass}>
       <Row>
-        <Col xs="1" lg="2"></Col>
-        <Col xs="10" lg="8" className='p-2'>
+        <Col xs="0" md="1" lg="2"></Col>
+        <Col xs="12" md="10" lg="8" className='p-2'>
           <div className='p-2 searchbar-main' style={{position:"relative"}}>
             <Row>
-              <Col xs="4" sm="3" className='text-center my-auto'>
-              <SelectBox
-                  className="font-medium text-blue_gray_900 text-left text-sm w-[9%] md:w-full"
-                  placeholderClassName="text-blue_gray_900"
-                  isMulti={false}
-                  name="group105"
-                  options={[
-                    { label: "New Delhi", value: "New Delhi" },
-                    { label: "Bangalore", value: "Bangalore" },
-                    { label: "Mumbai", value: "Mumbai" },
-                  ]}
-                  isSearchable={false}
-                  placeholder="New Delhi"
-              />
+              <Col xs="4" sm="3" className='text-center my-auto p-0'>
+                <SelectBox
+                    className="font-medium text-blue_gray_900 text-left text-sm w-[9%] md:w-full"
+                    placeholderClassName="text-blue_gray_900"
+                    isMulti={false}
+                    name="group105"
+                    options={[
+                      { label: "New Delhi", value: "New Delhi" },
+                      { label: "Bangalore", value: "Bangalore" },
+                      { label: "Mumbai", value: "Mumbai" },
+                    ]}
+                    isSearchable={false}
+                    placeholder="New Delhi"
+                    value="New Delhi"
+                />
               </Col>
               <Col xs="8" sm="9" style={{borderLeft: "1px solid #0D2855", position:"relative"}} className='my-auto'>
-                <div style={{zIndex:"2"}}>
-                  <input className='searchbar-input' value={squery} onClick={() => setShowSuggestions(true)} onChange={(e) => setSquery(e.target.value)} placeholder='Search for loacality, Landmark, Project or Builder' />
-                  <button className='btn-info float-end' onClick={() => navigate("/properties")}><span className='expert-btn-text px-3'>Search</span></button>
+                <div style={{zIndex:"2", display:"flex"}}>
+                  <div style={{flexGrow:"100"}}>
+                    <input className='searchbar-input' value={squery} onClick={() => setShowSuggestions(true)} onChange={(e) => setSquery(e.target.value)} placeholder={ window.innerWidth > 650 ? "Search for loacality, Landmark, Project or Builder":"Type here"} />
+                  </div>
+                  <button className='btn-info float-end px-2 py-1' onClick={() => navigate("/properties")}><span className='expert-btn-text px-1 px-lg-3'>Search</span></button>
                   {showSuggestions && <div className='search-suggestions'>
                     <span  style={{opacity:"0.5", padding:"7px 24px", ...getFontData("12px", "400")}}>POPULAR SEARCH</span>
                     {searchSuggestions.map((item) => (
@@ -66,8 +69,7 @@ export default function Searchbar({defaultClass="searchbar-setter"}) {
               </Col>
             </Row>
             {defaultClass != "" && 
-              <div className='searchbar-text p-3 d-block d-lg-none mt-4'>
-                <br/>
+              <div className='searchbar-text px-3 px-sm-5 px-md-5 d-lg-none mt-4 pb-3' style={{paddingTop:"36px"}}>
                 <span className='searchbar-text-bold'>Buy</span>
                 <span>Rent</span>
                 <span>Commercial</span>
@@ -77,7 +79,7 @@ export default function Searchbar({defaultClass="searchbar-setter"}) {
             }
           </div>
         </Col>
-        <Col xs="1" lg="2"></Col>
+        <Col xs="0" md="1" lg="2"></Col>
       </Row>
     </div>
   )
