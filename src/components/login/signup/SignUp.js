7226de2a492c9d7d5ece14cea7508deefd756d0d
Mayuri Gupta <mg153@snu.edu.in>
2023-07-09 22:21:06 +0530

    if aplied, fixes all issues

--- src/components/login/signup/SignUp.js
+++ src/components/login/signup/SignUp.js
@@ -3,96 +3,71 @@ import Owner from "../owner/Owner1";
 import Customer from "../customer/Customer1";
 import "./SignUp.css";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import LoginFooter from "../LoginFooter";
 
-function SignUp() {
-  const [Button, setButton] = useState("");
+function SignUp(props) {
+  const [signupType, setSignupType] = useState("");
+  const handleBack = () => {
+    setSignupType("")
+  }
   return (
-    <div class="row login">
-      {Button === "Owner" && (
-        <div class="col" style={{ textAlign: "right", padding: 0 }}>
-          <img src="images/image 6.png" alt="Not found!" />
-        </div>
-      )}
-      {Button === "Customer" && (
-        <div class="col" style={{ textAlign: "right", padding: 0 }}>
-          <img src="images/Group 42.png" alt="Not found!" />
-        </div>
-      )}
-      <div
-        class={Button === "" ? "col d-flex justify-content-center" : "col"}
-        style={{ padding: 0 }}
-      >
-        <div className="form">
-          {/* {Button !== "" && (
-            <div>
-              <button
-                type="click"
-                class="btn btn btn2"
-                onClick={() => setButton("")}
-              >
-                <NavigateBeforeIcon
-                  style={{ padding: "0.5vh", marginLeft: "0vh" }}
-                />
-                Back
-              </button>
-            </div>
-          )} */}
-          <div style={{ textAlign: "center" }}>
-            <img src="images/loginPropscan.png" alt="Not found!" />
-          </div>
-          {Button === "Owner" ? (
-            <Owner></Owner>
-          ) : Button === "Customer" ? (
-            <Customer></Customer>
-          ) : (
-            <div>
-              <div className="mb-3" style={{ marginTop: "2.5vh" }}>
-                <p className="paragraph">Your Trusted Real Estate Partner</p>
+    <div>
+      {
+        signupType === 'customer' ? <Customer handleBack={handleBack}/>
+        :
+        signupType === 'broker' ? <Owner />
+        :
+        <div class="row login">
+        <div
+          class={"col d-flex justify-content-center"}
+          style={{ padding: 0 }}
+        >
+        <div className="login-form">
+          <div>
+              <div style={{width:"100%", textAlign:"left", marginLeft:"21px", padding:0}}>
+                <button
+                  type="click"
+                  class="btn back-btn2"
+                  onClick={() => props.handleBack()}
+                >
+                  <NavigateBeforeIcon
+                    style={{ padding: 0, margin:0, fontSize:"18px" }}
+                  />
+                  Back
+                </button>
               </div>
-              <div className="p2">Continue as</div>
-              <div style={{ textAlign: "center", margin: "1vw" }}>
+              <div style={{width:"100%", textAlign:"center"}}>
+                <img src="images/loginPropscan.png" alt="Not found!" />
+              </div>
+          </div>
+            <div>
+              <div className="mb-3" style={{ marginTop: "15px" }}>
+                <p className="info-paragraph">Your Trusted Real Estate Partner</p>
+              </div>
+              <div className="continue-text">Continue as</div>
+              <div style={{ textAlign: "center", marginTop:'64px'}}>
                 <button
                   className="btn1"
                   type="submit"
-                  onClick={() => setButton("Owner")}
+                  onClick={() => setSignupType("customer")}
                 >
-                  OWNER/BROKER
+                  Customer
                 </button>
                 <button
                   className="btn1"
                   type="submit"
-                  onClick={() => setButton("Customer")}
+                  onClick={() => setSignupType("broker")}
                 >
-                  CUSTOMER
+                  Owner/Broker
                 </button>
               </div>
             </div>
-          )}
-          <div className="icon_div">
-            <img
-              src="images/facebook_icon.png"
-              alt="Not found!"
-              style={{ height: "3vh", width: "auto" }}
-            />
-            <img
-              src="images/Twitter_icon.png"
-              alt="Not found!"
-              style={{ height: "4vh", width: "auto", marginTop: "1vh" }}
-            />
-            <img
-              src="images/Insta_icon.png"
-              alt="Not found!"
-              style={{ height: "3vh", width: "auto" }}
-            />
-            <img
-              src="images/LinkedIn_icon.png"
-              alt="Not found!"
-              style={{ height: "4.2vh", width: "auto", marginTop: "1.2vh" }}
-            />
+          <LoginFooter />      
           </div>
-        </div>
       </div>
     </div>
+}
+  </div>
   );
 }
 
